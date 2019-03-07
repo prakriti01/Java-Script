7385c2894e103a8b521d1d45a0a229955f6ed7cc
Rana <prakritirana00@gmail.com>
2019-03-07 18:18:00 -0500

    Final Commit

--- 14-Intro-To-JavaScript/Instructions/StarterCode/static/app1.js
+++ 14-Intro-To-JavaScript/Instructions/StarterCode/static/app1.js
@@ -1,5 +1,3 @@
 // from data.js
 //var tableData = data;
 
-
-
---
+++ 14-Intro-To-JavaScript/Instructions/StarterCode/static/js/app1.js
@@ -0,0 +1,17 @@
+// from data.js
+//var tableData = data;
+
+// Get a reference to the table body
+// var tbody = d3.select("tbody");
+
+// Console.log the weather data from data.js
+//console.log(data);
+
+function Person(firstName, lastName) {
+  this.firstName = firstName;
+  this.lastName = lastName;
+}
+
+var me = new Person("John", "Smith");
+
+console.table(me);
